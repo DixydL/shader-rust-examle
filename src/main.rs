8bf9f328fef37eff23aa1a02f5e0cd7381ff2b5a
dixyd <diexeed@gmail.com>
2018-03-09 13:23:07 +0200

    matrix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 extern crate gl;
 extern crate glutin;
-
-
 use std::ffi::{CString, CStr};
-
+use std::ops::Mul;
 use std::mem;
 use gl::types::*;
 use std::ptr;
@@ -51,12 +49,64 @@ mod calculate {
 }
 
 mod matrix {
-    struct Matrix {
-
+    #[derive(Debug)]
+    pub struct Matrix {
+       mat1 :[f32;4],
+       mat2 :[f32;4],
+       mat3 :[f32;4],
+       mat4 :[f32;4],
+       mat5 :[f32;4],
+       mat6 :[f32;4],
+       mat7 :[f32;4],
+       mat8 :[f32;4],
     }
-    impl Matrix {
-
+    impl Matrix{
+        pub fn mat4
+        (
+            m11 :f32, m12:f32, m13: f32, m14: f32,
+            m21 :f32, m22:f32, m23: f32, m24: f32,
+            m31 :f32, m32:f32, m33: f32, m34: f32,
+            m41 :f32, m42:f32, m43: f32, m44: f32
+        ) -> Matrix
+        {
+          Matrix {
+              mat1 : [m11,m21,m31,m41],
+              mat2 : [m12,m22,m32,m42],
+              mat3 : [m13,m23,m33,m43],
+              mat4 : [m14,m24,m34,m44],
+              ////////////////////////
+              mat5 : [m11,m21,m31,m41],
+              mat6 : [m21,m22,m23,m24],
+              mat7 : [m31,m32,m33,m34],
+              mat8 : [m41,m42,m43,m44],
+          }
+        }
+        pub fn vector_mul(vec1:&[f32],vec2:&[f32]) -> f32{
+            let mut m_index = 0;
+            let mut m = 0.;
+            for i in 0..4 {
+                 m = m + (vec1[i] * vec2[i]);
+            }
+            m
+        }
+        pub fn matrix_mul (mat1: Matrix, mat2: Matrix) -> Vec<Vec<f32>> {
+            let mut vec = Vec::new();
+            for i in 0..4 {
+                let m1 = Matrix::vector_mul(&mat1.mat1,&mat2.mat5);
+                let m2 = Matrix::vector_mul(&mat1.mat2,&mat2.mat6);
+                let m3 = Matrix::vector_mul(&mat1.mat3,&mat2.mat7);
+                let m4 = Matrix::vector_mul(&mat1.mat4,&mat2.mat8);
+                vec.push(vec![m1,m2,m3,m4]);
+            }
+            println!("{:?},",vec);
+            vec
+        }
     }
+//    impl Mul for Matrix {
+//        fn mul(self, other: Self){
+//
+//        }
+//    }
 }
 mod shader_mod {
 
@@ -66,12 +116,7 @@ mod shader_mod {
         layout (location = 0) in vec2 position;
         uniform mat4 model;
         void main() {
-            gl_Position = mat4(
-            1.0, 0.0, 0.0, 0.0,
-            0.0, 1.0, 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0,
-            0.0, 0.0, 0.0, 1.0
-            ) * vec4(position.x,position.y, 1.0, 1.0);
+            gl_Position = model * vec4(position.x,position.y, 1.0, 1.0);
         }
     "
     ;
@@ -121,13 +166,21 @@ fn main() {
     let mut VAO : GLuint = 0;
     let mut shader_program: GLuint = 0;
 
-    let mat: [f32;16] =
-        [
-            1.0,0.0,0.0,0.0,
-            0.0,1.0,0.0,0.0,
-            0.0,0.0,1.0,0.0,
-            0.0,0.0,0.0,1.0
-        ];
+    let mat1 = matrix::Matrix::mat4(
+        1.0,0.0,0.0,0.0,
+        0.0,1.0,0.0,0.0,
+        0.0,0.0,1.0,0.0,
+        0.0,0.0,0.0,1.0,
+    );
+    let mat2 = matrix::Matrix::mat4(
+        1.0,0.0,0.0,0.0,
+        0.0,1.0,0.0,0.0,
+        0.0,0.0,1.0,0.0,
+        0.0,0.0,0.0,1.0,
+    );
+
+    matrix::Matrix::matrix_mul(mat1,mat2);
+    let mat = [0.,0.1] ;
     unsafe {
         let vertex_shader : GLuint = gl::CreateShader(gl::VERTEX_SHADER);
         let vShaderCode = CString::new(shader_mod::SHADER_VERTEX.as_bytes()).unwrap();
